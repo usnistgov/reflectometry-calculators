9523a112a9748c54f7439c33ca87ec3a3c8309df
Brian Maranville <brian.maranville@nist.gov>
2014-07-01 11:19:15 -0400

    reversing the order of layers in refl1d script generator

--- js/generate_slab_script.js
+++ js/generate_slab_script.js
@@ -1,6 +1,9 @@
 var prec = 5; // precision
 
 generate_slab_script = function(sldarray, filename) {
+    // sldarray order is based on the old reflpak ordering (beam source side first)
+    // while refl1d builds the slab model from the "bottom", with the substrate slab first
+    sldarray.reverse();
     py = "";
     var filename = filename || "myfile.refl";
     
